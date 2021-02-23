ca5a15d0badb63f14b0920a5b556ccf7bfb0359a
LittleMoon215 <littlemoon215@mail.ru>
2021-02-24 03:02:32 +0700

    Переворотил вьюху главной страницы. Изменен шаблон

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.utils.text import slugify
@@ -14,23 +15,23 @@ from todo import numbersAPI
 from todo import weatherAPI
 
 def task(request):
-    if request.method == "POST":
-        if "Add" in request.POST:
-            title = request.POST["title"]
-            description = request.POST["description"]
-            # date_created = str(request.POST["date_created"])
-            date_end = str(request.POST["date_end"])
-            status = request.POST["status"]
-            author = request.POST["author"]
-            created_task = Task(title=title, description=description, date_end=date_end,
-                                status=status, author=author)
-            created_task.save()
-            return redirect("/")
-        if "Delete" in request.POST:
-            checkedlist = request.POST.getlist('checkedbox')
-            for i in range(len(checkedlist)):
-                tasks = Task.objects.filter(id=int(checkedlist[i]))
-                tasks.delete()
+    # if request.method == "POST":
+    #     if "Add" in request.POST:
+    #         errors = ""
+    #         title = request.POST["title"]
+    #         description = request.POST["description"]
+    #         date_end = str(request.POST["date_end"])
+    #         status = request.POST["status"]
+    #         author = "Guest"
+    #         created_task = Task(title=title, description=description, date_end=date_end,
+    #                                 status=status, author=author)
+    #         created_task.save()
+    #         return redirect("/")
+    #     if "Delete" in request.POST:
+    #         checkedlist = request.POST.getlist('checkedbox')
+    #         for i in range(len(checkedlist)):
+    #             tasks = Task.objects.filter(id=int(checkedlist[i]))
+    #             tasks.delete()
     tasks = Task.objects.all()
     return render(request, "home.html", {"tasks": tasks, "statuses": Task.STATUS_CHOICES})
 
